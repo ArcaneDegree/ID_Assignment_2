568759205c8b5805168a820ad4bb78314ba76138
astronny <aar0planee@gmail.com>
2023-02-05 20:08:20 +0800

    small update to js script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function() {
+swift = document.querySelector(".swift");
+swift.onclick = function() {
     navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active");
 }
