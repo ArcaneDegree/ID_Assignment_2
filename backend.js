d31f5e98fd2a34c3c5ec9fd71cce3bbc9e94cb0f
ArcaneDegree <s10239861@connect.np.edu.sg>
2023-02-06 09:44:05 +0800

    Update Log - Removed dbg_mode variable. - Implemented a new function called SearchAllDirsForFile, which takes in a file name as the 1st parameter and the path of the directory to start the search from as the 2nd parameter. This function recursively searches for a file with the same name as the selected file name (parameter 1), looking through all files and directories under the directory at the specified starting directory path (parameter 2), as well as subdirectories under those directories if required, this function returns "" (an empty string) if no file was found at all, otherwise it returns a string containing the file path of the file with the same name as the selected file name. - Fixed bug causing the FileSystem.statSync method to throw an error stating that the file or folder was not found. - Implemented the HandleRequest function, this function serves as a callback function that is called when a client sends a request to the server. The primary purpose of this function is to send back a response to the client depending on the request that is sent by the client to the server. - Implemented procedures for handling requests for files (e.g. HTML files, CSS files and JS files) that could be sent when a webpage is being loaded, as well as for handling requests for files that did not exist on the server. - Removed a console.log statement that would be run whenever a request had been sent to the server. - Fixed a bug causing the response header to not be filled in even if the file being requested was a HTML, CSS or JS file. - Fixed a bug causing an incorrect path to be passed into the FileSystem.readFileSync method when the client requested the HTML file for the landing page.

--- backend.js
+++ backend.js
@@ -4,8 +4,6 @@ var FileSystem = require("fs");
 
 var http_server = null;
 
-var dbg_mode = true;
-
 var port_num = 8080;
 
 var host_name = "localhost";
@@ -14,45 +12,98 @@ var current_working_dir_path = "C:/Poly/Interactive_Development_Y1S2/ASG2/ID_Ass
 
 function OnHttpServerStartListening()
 {
-    if (dbg_mode == true)
+    console.log("Started listening on port " + port_num + ".");
+}
+
+// Recursively searches for a file with the same name as the selected file name (parameter 1), looking
+// through all files and directories under the specified directory path (parameter 2), as well as
+// subdirectories under those directories if required, returns "" if no file was found at all, 
+// otherwise returns a string containing the file path of the file with the same name as the 
+// selected file name.
+function SearchAllDirsForFile(file_name, working_dir_path)
+{
+    // console.log(`Checking directory at path '${working_dir_path}' for '${file_name}' file.`);
+
+    let result_file_path = "";
+
+    let working_dir_path_file_list = FileSystem.readdirSync(working_dir_path);
+
+    for (let currentFileIndex = 0; currentFileIndex < working_dir_path_file_list.length; currentFileIndex++)
     {
-        console.log("Started listening on port 8080.");
+        if (working_dir_path_file_list[currentFileIndex] == ".git" || working_dir_path_file_list[currentFileIndex] == ".gitignore")
+        {
+            continue;
+        }
+
+        if (working_dir_path_file_list[currentFileIndex] == file_name)
+        {
+            result_file_path = working_dir_path + "/" + file_name;
+
+            break;
+        }
+
+        if (FileSystem.statSync(working_dir_path + "/" + working_dir_path_file_list[currentFileIndex]).isDirectory() == true)
+        {
+            result_file_path = SearchAllDirsForFile(file_name, working_dir_path + "/" + working_dir_path_file_list[currentFileIndex]);
+
+            if (result_file_path != "")
+            {
+                break;
+            }
+        }
     }
+
+    // console.log(`Result file path obtained: '${result_file_path}'`);
+
+    return result_file_path;
 }
 
 function HandleRequest(request, response)
 {
-    if (request.url = "/")
-    {
-        if (dbg_mode == true)
-        {
-            console.log("Received initial request.");
-        }
+    // console.log(request.url);
 
+    if (request.url == "/")
+    {
         response.writeHead(200, {"Content-Type" : "text/html"});
 
-        response.write(FileSystem.readFileSync(current_working_dir_path + "/homepage.html"));
+        response.write(FileSystem.readFileSync(SearchAllDirsForFile("index.html", current_working_dir_path)));
 
         response.end();
     }
     else
     {
+        // console.log(`Handling request for file with name '${request.url.replace("/", "")}'.`);
+
+        // Procedure for handling requests for files (e.g. HTML files, CSS files and JS files) for 
+        // a webpage when it is being loaded.
+        // Sample request url for a file named 'script.js': /script.js
         let file_extension = request.url.split(".")[1];
 
-        if (file_extension == ".html")
+        if (file_extension == "html")
         {
             response.writeHead(200, {"Content-Type" : "text/html"});
         }
-        else if (file_extension == ".css")
+        else if (file_extension == "css")
         {
             response.writeHead(200, {"Content-Type" : "text/css"});
         }
-        else if (file_extension == ".js")
+        else if (file_extension == "js")
         {
             response.writeHead(200, {"Content-Type" : "application/javascript"});
         }
 
-        response.write(FileSystem.readFileSync(current_working_dir_path + request.url));
+        corresponding_file_path = SearchAllDirsForFile(request.url.replace("/", ""), current_working_dir_path);
+
+        if (corresponding_file_path == "")
+        {
+            response.statusCode = 404;
+
+            response.write("Status: Not Found.");
+        }
+        else
+        {
+            response.write(FileSystem.readFileSync(corresponding_file_path));
+        }
 
         response.end();
     }
--- homepage.js
+++
@@ -1,1 +0,0 @@
-alert("Homepage script started.");
