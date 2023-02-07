877faa24af4fdfc8831a79c34ad9117e3bf31947
astronny <aar0planee@gmail.com>
2023-02-08 04:15:52 +0800

    added login page and motto amongst other stuff

--- script.js
+++ script.js
@@ -6,6 +6,31 @@ swift.onclick = function() {
     navBar.classList.toggle("active");
 }
 
+var x=document.getElementById('login');
+		var y=document.getElementById('register');
+		var z=document.getElementById('btn');
+		function register()
+		{
+			x.style.left='-400px';
+			y.style.left='50px';
+			z.style.left='110px';
+		}
+		function login()
+		{
+			x.style.left='50px';
+			y.style.left='450px';
+			z.style.left='0px';
+		}
+
+        var modal = document.getElementById('login-form');
+        window.onclick = function(event) 
+        {
+            if (event.target == modal) 
+            {
+                modal.style.display = "none";
+            }
+        }        
+
 /*
 // For backend testing purposes only (Do not remove this, if necessary, you may comment this part out)
 // Start of backend testing part
