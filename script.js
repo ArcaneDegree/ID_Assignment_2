23339e28b35c5bf8df0a0820159f5338130f9cba
ArcaneDegree <s10239861@connect.np.edu.sg>
2023-02-10 07:15:29 +0800

    Update Log - Removed the call to the QueryRestDB function of the FastDoc API in the 'script.js' file.

--- script.js
+++ script.js
@@ -154,10 +154,12 @@ FastDocAPI.SendRequestToServerLocalDB("single", [
 FastDocAPI.AddNewMember("Carl Jung", "interestingpassword55473", "carl_jung@gmail.com");
 */
 
+/*
 FastDocAPI.QueryRestDB("member", "get_all", "", (request_obj) =>
 {
   console.log("Response status code: " + request_obj.status);
 
   console.log("Response received: " + request_obj.responseText);
 });
+*/
 // End of backend testing part
